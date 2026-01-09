8192b3893af071254ef936afd24aba84373a440e
Adi <adialon050@gmail.com>
2026-01-09 10:57:56 +0200

    Add multiple choice quiz block with frontend React rendering

--- plugins/are-you-paying-attention/build/index.asset.php
+++ plugins/are-you-paying-attention/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime'), 'version' => '2bdb146bb008d44bc443');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-components'), 'version' => '3ddbbf1089613a8a831c');
