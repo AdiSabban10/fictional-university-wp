6d4dbe88730a23a099f7361a38ec54e18ef123bd
Adi <adialon050@gmail.com>
2026-01-17 22:27:01 +0200

    Build solved-counter assets and update gitignore

--- plugins/solved-counter/build/index.asset.php
+++ plugins/solved-counter/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '545a3e8257ff39e07938');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '86974e522bb7fa18085e');
